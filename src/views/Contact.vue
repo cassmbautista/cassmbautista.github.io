23169ca611280c2c315a281cf8468b9c51dcacfa
Cassandra Bautista <cass.m.bautista@gmail.com>
2019-08-23 00:07:56 -0400

    other stuff

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,9 +3,9 @@
 	<h1>
 		Contact
 	</h1>
-	<i-form>
+	<i-form action="https://formspree.io/garberathena@gmail.com" method="POST" >
 	    <i-form-group>
-	        <i-input v-model="name" placeholder="Name" />
+	        <i-input v-model="name" placeholder="Name" name='name'/>
 	    </i-form-group>
 
 	    <i-form-group>
@@ -20,6 +20,17 @@
 	        <i-button type="submit">Submit</i-button>
 	    </i-form-group>
 	</i-form>
+
+	<!-- <form action="https://formspree.io/garberathena@gmail.com" method="post"> -->
+
+	  <!-- the redirect_to is optional, the form will redirect to the referrer on submission -->
+	  <!-- <input type='hidden' name='redirect_to' value='localhost:8000/home' /> -->
+
+	  <!-- all your input fields here.... -->
+<!-- 	  <input type='text' name='test' />
+
+	  <input type='submit' value='Test form' />
+	</form> -->
 </i-container>
 </template>
 
--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -3,7 +3,7 @@
     	<i-nav size="sm" class="_justify-content-center">
 	    	<i-nav-item class="_text-light" :to="{ name: 'home' }">Home</i-nav-item>
 	    	<i-nav-item class="_text-light" :to="{ name: 'gallery' }">Gallery</i-nav-item>
-	    	<i-nav-item class="_text-light" :to="{ name: 'contact' }">Contact</i-nav-item>
+	    	<!-- <i-nav-item class="_text-light" :to="{ name: 'contact' }">Contact</i-nav-item> -->
 		</i-nav>	
     	<p class="_font-size-sm _margin-top-0 _margin-bottom-0">Copyright 2019 &copy; Cassandra Bautista<p>
     </i-layout-footer>
