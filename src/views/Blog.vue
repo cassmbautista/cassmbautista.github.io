1ce3351d4b06860323db637efdd2f9b1018a90cf
Cassandra Bautista <cass.m.bautista@gmail.com>
2019-08-29 00:58:48 -0400

    wip

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,24 @@
 <template>
-<div>
+<i-container>
+	<h1>Blog Posts</h1>
+
+    <i-row class="_margin-bottom-3-4">
+      <i-column xl="3" lg="4" md="6" sm="8" xs="12">
+        <strong>Categories:</strong>
+        <i-select v-model="selectedFilter">
+          <i-select-option
+            v-for="(filter, filterIndex) in filters"
+            :label="getFilterLabel(filter)"
+            :key="'filter' + filterIndex"
+            :value="filter"
+          ></i-select-option>
+        </i-select>
+      </i-column>
+    </i-row>
+		<button @click="readMarkdownFile">
+			stuff
+		</button>
+	
 	<i-card v-for="card of cards">
 	    <!-- <img slot="image" src=".." alt="Card Image" /> -->
 	    <h4 class="title">{{card.title}}</h4>
@@ -9,7 +28,7 @@
 	    </p>
 	    <!-- <a class="link" href="http://inkline.io">Example Link</a> -->
 	</i-card>
-</div>
+</i-container>
 </template>
 
 <script>
@@ -17,44 +36,57 @@ module.exports = {
 	name: 'BlogPosts',
 	data: function() {
 		return {
-			cards: [
-			{
-				title: 'ajos',
-				subtitle: 'aoijfos',
-				description: 'asjdfoai'
-			},
-
-			{
-				title: 'ajos',
-				subtitle: 'aoijfos',
-				description: 'asjdfoai'
-			},
-			
-			{
-				title: 'ajos',
-				subtitle: 'aoijfos',
-				description: 'asjdfoai'
-			}
-			]
+      		allFiles: []
 		};
 	},
 	methods: {
-		
-    getGitTree: function () {
-      this.loading = true;
-      axios.get('https://api.github.com/repos/cassmbautista/cassmbautista.github.io/git/trees/master?recursive=1')
-        .then((response) => {
-          this.allFiles = response.data.tree;
+		getGitTree: function () {
+			this.loading = true;
+			axios.get('https://api.github.com/repos/cassmbautista/cassmbautista.github.io/git/trees/master?recursive=1')
+			.then((response) => {
+				this.allFiles = response.data.tree;
+				this.networkError = false;
+				this.loading = false;
+			})
+			.catch((err) => {
+				if (err) {
+					this.networkError = true;
+				}
+				this.loading = false;
+			});
+		},
+		readMarkdownFile: function () {
+			this.blogFiles.
+      axios.get(this.blogFiles[0].path)
+        .then(function (response) {
+					console.log(response.data)
+          this.content = marked(response.data);
           this.networkError = false;
-          this.loading = false;
-        })
-        .catch((err) => {
+        }.bind(this))
+        .catch(function (err) {
           if (err) {
             this.networkError = true;
           }
+        }.bind(this))
+        .finally(function () {
           this.loading = false;
-        });
+        }.bind(this));
+		}
+	},
+	computed: {
+    blogFiles: function () {
+      return this.allFiles.filter(function (file) {
+        return !!(
+          file.path.startsWith('src/blog/') &&
+          file.path.endsWith('.md')
+        );
+      });
     }
-	}
+	},
+  created: function () {
+    if (!this.allFiles.length) {
+      this.getGitTree();
+    }
+  }
 };
 </script>
